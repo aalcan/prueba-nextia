0f4e67aba747578057d6ece8dc482b8cd211df38
Armando Alcantara <armandt32@gmail.com>
2022-11-04 22:15:40 -0600

    DB: Seed db with default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +19,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = new User([
+            'name' => "Armando Alcantara Lagunas",
+            'username' => "test",
+            'password' => Hash::make('ArmandoAlcantaraLagunas')
+        ]);
+        $user->save();
     }
 }
